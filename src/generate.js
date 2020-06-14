0836304d484a2c86effc2b60d45c7a0a1987a4f6
Emily Compton <emcompton17@hotmail.com>
2020-06-14 12:13:05 -0400

    final fixes

--- src/generate.js
+++ src/generate.js
@@ -5,7 +5,7 @@ import store from '/src/store.js';
 const homePage = function(bookmarkListHTML) {
   console.log('generate function');
   let homepageHTML = `<header>
-  <h1>My Bookmark App</h1>
+  <h1>My Bookmarks App</h1>
   <p>All My Favorite Websites Bookmarked Here!</p>
   </header>
 <button class="js-add-bookmark">Add New Bookmark</button>
